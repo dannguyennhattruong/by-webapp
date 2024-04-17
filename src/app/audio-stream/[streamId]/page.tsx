71b8d60677964f2500b958808f0821a7509f38e5
Khánh Đan Nguyễn Nhật <dan.nhatkhanh@gmail.com>
2024-04-17 18:36:06 +0700

    move audio to route "/audio-stream/[streamId]"

---
+++ src/app/audio-stream/[streamId]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AudioStreamComponent from "../../components/AudioStream/audiostream.component";
+
+function AudioStreamPage() {
+  return <AudioStreamComponent />;
+}
+
+export default AudioStreamPage;
